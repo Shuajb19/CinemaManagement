0b50db285b336263b322a58163f8095a40647471
shuajb <shuajbi361@gmail.com>
2022-02-22 14:05:39 +0100

    Created AddAsync files

--- CinemaManagementSystem/Data/Base/EntityBaseRepository.cs
+++ CinemaManagementSystem/Data/Base/EntityBaseRepository.cs
@@ -8,36 +8,25 @@ namespace CinemaManagementSystem.Data.Base
 {
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
-        private readonly AppDbContext _context;
+        private readonly ApplicationDbContext _context;
 
-        public EntityBaseRepository(AppDbContext context)
+        public EntityBaseRepository(ApplicationDbContext context)
         {
             _context = context;
         }
-        public Task AddAsync(T entity)
-        {
-            throw new NotImplementedException();
-        }
+
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
 
         public Task DeleteAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<T>> GetAllAsync()
-        {
-            var results = await _context.Set<T>().ToListAsync();
-            return results;
-        }
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
 
-        public async Task<T> GetByIdAsync(int id)
-        {
-            var results = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
-            return results;
-        }
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
 
         
-
         public Task<T> UpdateAsync(int id, T entity)
         {
             throw new NotImplementedException();
--- CinemaManagementSystem/Data/Base/IEntityBaseRepository.cs
+++ CinemaManagementSystem/Data/Base/IEntityBaseRepository.cs
@@ -9,7 +9,7 @@ namespace CinemaManagementSystem.Data.Base
     public interface IEntityBaseRepository<T> where T: class, IEntityBase, new()
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetIdAsync(int id);
+        Task<T> GetByIdAsync(int id);
         Task AddAsync(T entity);
         Task<T> UpdateAsync(int id, T entity);
         Task DeleteAsync(int id);
