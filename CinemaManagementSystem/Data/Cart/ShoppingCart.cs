2349c261f1e798f0a820b7cf4b209fde085fe330
Leonora <leonoragerguri1@gmail.com>
2022-03-02 11:41:33 +0100

    Created ShoppingCart Class

---
+++ CinemaManagementSystem/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿using CinemaManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaManagementSystem.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public ApplicationDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set;}
+        
+        public ShoppingCart(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId ==
+            ShoppingCartId).Include(n => n.Movie).ToList());
+        }
+        public double GetShoppingCartTotal() =>_context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n =>
+            n.Movie.Price * n.Amount).Sum();
+            
+        }
+}
+
