6a64b619b2c62399e440aea7e24212de4f08ebe1
er48730 <er48730@ubt-uni.net>
2022-03-03 14:14:31 +0100

    Adding OrdersController file

---
+++ CinemaManagementSystem/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using CinemaManagementSystem.Data.Cart;
+using CinemaManagementSystem.Data.Services;
+using CinemaManagementSystem.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaManagementSystem.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            
+            return View(response);
+        }
+    }
+}
