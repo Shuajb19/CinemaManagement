a4b622298a703df7c8b9c268ef7d339aae2e04a9
shuajb <shuajbi361@gmail.com>
2022-04-01 15:33:42 +0200

    Created crud for coupon

---
+++ CinemaManagementSystem/Models/Coupon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaManagementSystem.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double Discount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MovieId { get; set; }
+        [ForeignKey("MovieId")]
+        public Movie Movies { get; set; }
+    }
+}
